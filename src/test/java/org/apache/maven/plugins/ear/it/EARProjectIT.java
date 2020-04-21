fd82a833f5acaad7f26a291a7f92aaadd49077d2
Karl Heinz Marbaise <khmarbaise@apache.org>
2020-04-21 23:48:34 +0200

    Updated.

--- src/test/java/org/apache/maven/plugins/ear/it/EARProjectIT.java
+++ src/test/java/org/apache/maven/plugins/ear/it/EARProjectIT.java
@@ -38,6 +38,7 @@ import static com.soebes.itf.extension.assertj.MavenITAssertions.assertThat;
 @MavenIT
 @DisplayName("EAR Project Integration tests.")
 @MavenPredefinedRepository
+@MavenRepository
 class EARProjectIT {
 
   @MavenTest
